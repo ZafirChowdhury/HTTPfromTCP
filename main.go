e899141ab62f34769d9c9bff36f55a0d70ddd4d8
ZafirChowdhury <zafirchowdhury69@gmail.com>
2026-05-26 22:45:14 +0600

    Done Newlines

--- main.go
+++ main.go
@@ -3,31 +3,44 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Println(err.Error())
 		return
 	}
 	defer file.Close()
 
 	buffer := make([]byte, 8)
+	var line string
 	for {
 
 		n, err := file.Read(buffer)
 
 		if err == io.EOF {
+			fmt.Printf("read: %s\n", line)
 			return
 		}
 
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 			return
 		}
 
-		fmt.Printf("read: %s\n", string(buffer[:n]))
+		currentIta := string(buffer[:n])
+		i := strings.IndexByte(currentIta, '\n')
+		if i == -1 {
+			line += currentIta
+			continue
+		}
+
+		line += currentIta[:i]
+		fmt.Printf("read: %s\n", line)
+		line = currentIta[i+1:]
 	}
 }
